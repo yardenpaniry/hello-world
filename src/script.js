46f1780077b2284d69b8072784b7c2b0b5aba443
yardenpaniry <50630420+yardenpaniry@users.noreply.github.com>
2022-06-26 19:21:05 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,30 @@
-const message = 'Hello world' // Try edit me
-
-// Update header text
-document.querySelector('#header').innerHTML = message
-
-// Log to console
-console.log(message)
+var workersDetails   = [{
+    name: "Joe Biden",
+    image: "https://tinyurl.com/ms854m44",
+    age: 30,
+    startDate: "1.1.2022"
+  },
+  {
+    name: "Elon Musk",
+    image: "https://tinyurl.com/ms854m44",
+    age: 25,
+    startDate: "16.2.2022"
+  },
+  {
+    name: "Yarden Paniry",
+    image: "https://tinyurl.com/ms854m44",
+    age: 29,
+    startDate: "16.7.2022"
+  }
+];
+var k = '<tbody>'
+for (i = 0; i < workersDetails.length; i++) {
+  k += '<tr>';
+  k += '<td>' + workersDetails[i].name + '</td>';
+  k += '<td><img src="' + workersDetails[i].image + '"></td>';
+  k += '<td>' + workersDetails[i].age + '</td>';
+  k += '<td>' + workersDetails[i].startDate + '</td>';
+  k += '</tr>';
+}
+k += '</tbody>';
+document.getElementById('tableData').innerHTML = k;
